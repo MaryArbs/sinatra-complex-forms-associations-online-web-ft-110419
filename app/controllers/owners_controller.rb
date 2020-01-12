8b79fe8c4ac3b0281104b4d76f6e3672ed58830c
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-12 20:27:02 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -15,7 +15,8 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
      @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-     redirect "owners/#{@owner.id}"
+    @owner.save
+  redirect to "owners/#{@owner.id}"
    end
 
   get '/owners/:id/edit' do
